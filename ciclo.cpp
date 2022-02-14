38c09eb101657b33f252b722ede1bbb42a03d4e0
lai2-01 <lai2-01@itismeucci.com>
2022-02-14 12:20:35 +0100

    01 branch master

---
+++ ciclo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int conta = 0;
+	int var_for = 10;
+
+for(int i = 0; i < var_for; i++){
+
+	conta++;
+	cout<<"Giro numero: "<<conta<<";";
+
+}
+
+return 0;
+}
